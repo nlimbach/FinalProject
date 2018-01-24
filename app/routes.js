09420f2befad72407719577f3c822f5c9dde2dc3
Charlie Arrendale <carrendale@gmail.com>
2018-01-23 20:40:54 -0500

    wip

--- app/routes.js
+++ app/routes.js
@@ -4,28 +4,6 @@ var connection = mysql.createConnection(dbconfig.connection, function(err){
     if(err)throw err;
 });
 
-// function handleDisconnect() {
-//     connection = mysql.createConnection(dbconfig); // Recreate the connection, since
-//                                                     // the old one cannot be reused.
-//
-//     connection.connect(function(err) {              // The server is either down
-//         if(err) {                                     // or restarting (takes a while sometimes).
-//             console.log('error when connecting to db:', err);
-//             setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//         }                                     // to avoid a hot loop, and to allow our node script to
-//     });                                     // process asynchronous requests in the meantime.
-//                                             // If you're also serving http, display a 503 error.
-//     connection.on('error', function(err) {
-//         console.log('db error', err);
-//         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//             handleDisconnect();                         // lost due to either server restart, or a
-//         } else {                                      // connnection idle timeout (the wait_timeout
-//             throw err;                                  // server variable configures this)
-//         }
-//     });
-// }
-//
-// handleDisconnect();
 
 connection.query('USE ' + dbconfig.database);
 
@@ -104,7 +82,7 @@ module.exports = function(app, passport) {
     //routes to authenticate profile and placeorder pages
 
     app.get('/profile', isAuthenticated, function(req, res) {
-        connection.query("SELECT * FROM orders WHERE username = ?",[req.user.username], function(err, data) {
+        connection.query("SELECT * FROM finalprojectorders WHERE username = ?",[req.user.username], function(err, data) {
             console.log(err);
             if (err) {
                 return res.status(500).end();
